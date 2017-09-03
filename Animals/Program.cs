d0fea25aba585982a2e0b08f94b162f2590c71cc
temskiy <temskiy@gmail.com>
2017-09-03 20:57:33 +0700

    Начал набивать костяк игры

--- Animals/Program.cs
+++ Animals/Program.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Animals
 {
     class Program
     {
-        private string[] b;
-        private string[] o;
-        private string[] p;
-
+        static string[] b;
+        static string[] o;
+        static string[] p;
+        static int i = 0;
         static void Main(string[] args)
         {
+            InitGame();
+
+        }
+
+        static void InitGame()
+        {
+            o[i] = "Кот";
+        }
+
+        static bool TryAndWaitAnswer(string animal)
+        {
+            Console.WriteLine("Это " + animal + ", да?");
+            var readLine = Console.ReadLine();
+            return readLine != null && (readLine.ToLower() == "да" ? true : false);
+
+        }
+
+        private static bool TryAgain()
+        {
+            Console.WriteLine("Сыграем ещё?");
+            var readLine = Console.ReadLine();
+            return readLine != null && (readLine.ToLower() == "да" ? true : false);
         }
     }
 }
