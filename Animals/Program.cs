76b92b6216788a7ebb59622424a21508f52f6e8e
temskiy <temskiy@gmail.com>
2017-09-03 19:44:39 +0700

    Добавьте файлы проекта.

---
+++ Animals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Animals
+{
+    class Program
+    {
+        private string[] b;
+        private string[] o;
+        private string[] p;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
